4b711d5263ff2c59e065a3cc066410326e897519
github username <github email address>
2018-05-25 17:38:05 -0400

    Update Results Function

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather report',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  var addressEncode = encodeURIComponent(argv.address)
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressEncode}&key=`,
   json: true
 }, (error, response, body) => {
-  console.log(body)
+  if (error) {
+    console.log("Unable to connect to Google")
+  } else if (body.status === "ZERO_RESULTS") {
+    console.log("Zero Results for given address")
+  } else if (body.status === "OK"){
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  } else {
+    console.log("WHAT?")
+  }
 })
--- playground/callbacks.js
+++ playground/callbacks.js
@@ -12,3 +12,6 @@ var getUser = (id, callback) => {
 getUser(31, (user) => {
   console.log(user)
 });
+
+
+//AIzaSyCzDJSWxBbGQ_VdXT-aITZRQELquy1CqFcr
